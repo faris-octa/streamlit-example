e1affc1dbd09ddbaa3b281ecd53f8570d07ca11c
Faris Octa <31800666+faris-octa@users.noreply.github.com>
2023-03-09 13:49:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ with col2:
                                   format_func=lambda x: '' if x == "0.  " else x,
                                   disabled = st.session_state.pic_to_remove == ' ',
                                   key='sample_to_remove')
-            deleted_row = int(df[df['info']==sampel].index.to_list())
+            deleted_row = df[df['info']==sampel].index[0]
         
         submitted = st.button("Submit")
         if submitted:
